7ad8a46210e9d96268b1db95a314de74662b09e5
David Greenland <David.Greenland@brighthr.com>
2024-11-21 09:09:29 +0000

    formatting

--- MusicSchool/Controllers/InstrumentController.cs
+++ MusicSchool/Controllers/InstrumentController.cs
@@ -2,46 +2,45 @@
 using Microsoft.EntityFrameworkCore;
 using MusicSchool.Responses;
 
-namespace MusicSchool
+namespace MusicSchool;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InstrumentController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class InstrumentController : ControllerBase
+    private readonly MusicSchoolDBContext _context;
+
+    public InstrumentController(MusicSchoolDBContext context)
     {
-        private readonly MusicSchoolDBContext _context;
+        _context = context;
+    }
 
-        public InstrumentController(MusicSchoolDBContext context)
+    // GET: api/Instrument
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<InstrumentResponse>>> GetInstruments()
+    {
+        var instruments = await _context.Instrument
+            .OrderBy(s => s.Name)
+            .Select(x => new InstrumentResponse(x.Id, x.Name, x.Category.CategoryName))
+            .ToListAsync();
+
+        return Ok(instruments);
+    }
+
+    // GET: api/Instrument/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<InstrumentResponse>> GetInstrument(int id)
+    {
+        var instrument = await _context.Instrument
+            .Where(x => x.Id == id)
+            .Select(x => new InstrumentResponse(x.Id, x.Name, x.Category.CategoryName, x.Students))
+            .ToListAsync();
+
+        if (instrument == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: api/Instrument
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<InstrumentResponse>>> GetInstruments()
-        {
-            var instruments = await _context.Instrument
-                .OrderBy(s => s.Name)
-                .Select(x => new InstrumentResponse(x.Id, x.Name, x.Category.CategoryName))
-                .ToListAsync();
-
-            return Ok(instruments);
-        }
-
-        // GET: api/Instrument/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<InstrumentResponse>> GetInstrument(int id)
-        {
-            var instrument = await _context.Instrument
-                .Where(x => x.Id == id)
-                .Select(x => new InstrumentResponse(x.Id, x.Name, x.Category.CategoryName, x.Students))
-                .ToListAsync();
-
-            if (instrument == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(instrument);
-        }
+        return Ok(instrument);
     }
 }
